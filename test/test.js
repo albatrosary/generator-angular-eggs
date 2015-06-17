27905a6bb1fa0fb87edd10b4e266040f5961d534
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-06-17 09:56:46 +0900

    add test pattern

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ var assert = require('yeoman-generator').assert;
 var _ = require('lodash');
 
 describe('Angular Eggs generator', function () {
+
   // not testing the actual run of generators yet
   it('the generator can be required without throwing', function () {
     this.app = require('../app');
@@ -36,6 +37,91 @@ describe('Angular Eggs generator', function () {
       'app/index.html'
     ];
 
+    var expectedApp = [
+      'app/components/about/about.html',
+      'app/components/about/about.js',
+      'app/components/contact/contact.html',
+      'app/components/contact/contact.js',
+      'app/components/footer/footer.html',
+      'app/components/header/header.html',
+      'app/components/home/home.html',
+      'app/components/home/home.js',
+      'app/scripts/main.js',
+      'app/scripts/config.js',
+      'app/service/gruntfiles/gruntfiles.js'
+    ];
+
+    var expectedConfig = [
+      'config/autoprefixer.js',
+      'config/browserSync.js',
+      'config/clean.js',
+      'config/concurrent.js',
+      'config/copy.js',
+      'config/express.js',
+      'config/filerev.js',
+      'config/htmlmin.js',
+      'config/imagemin.js',
+      'config/injector.js',
+      'config/jshint.js',
+      'config/karma.js',
+      'config/plato.js',
+      'config/protractor.js',
+      'config/run.js',
+      'config/svgmin.js',
+      'config/usemin.js',
+      'config/useminPrepare.js',
+      'config/watch.js',
+      'config/wiredep.js',
+      'config/yuidoc.js'
+    ];
+
+    var expectedE2e = [
+      'e2e/spec/exists.spec.js',
+      'e2e/pageObject.conf.js',
+      'e2e/protractor.conf.js'
+    ];
+
+    var expectedServer = [
+      'server/api/gruntfiles.js',
+      'server/data/app.json',
+      'server/app.js'
+    ];
+
+    var expectedTest = [
+      'test/components/about/about.spec.js',
+      'test/components/contact/contact.spec.js',
+      'test/components/home/home.spec.js',
+      'test/service/gruntfiles/gruntfiles.mock.js',
+      'test/service/gruntfiles/gruntfiles.spec.js',
+      'test/karma.conf.js',
+      'test/main.spec.js'
+    ];
+
+    var expectedDir = [
+      'app/components',
+      'app/components/about',
+      'app/components/contact',
+      'app/components/footer',
+      'app/components/home',
+      'app/images',
+      'app/scripts',
+      'app/service',
+      'app/styles',
+      'config',
+      'e2e',
+      'e2e/spec',
+      'server',
+      'server/api',
+      'server/data',
+      'test',
+      'test/components',
+      'test/components/about',
+      'test/components/contact',
+      'test/components/home',
+      'test/service',
+      'test/service/gruntfiles'
+    ];
+
     var options = {
       'skip-install-message': true,
       'skip-install': true,
@@ -53,100 +139,21 @@ describe('Angular Eggs generator', function () {
         .withGenerators([[helpers.createDummyGenerator(), 'mocha:app']]);
     });
 
-
-    console.log(path.join(__dirname, '../app'));  
-
     it('creates expected files', function (done) {
       runGen.withOptions(options).on('end', function () {
 
         assert.file([].concat(
           expected,
-          'app/components',
-          'app/components/about',
-          'app/components/about/about.html',
-          'app/components/about/about.js',
-          'app/components/contact',
-          'app/components/contact/contact.html',
-          'app/components/contact/contact.js',
-          'app/components/footer',
-          'app/components/footer/footer.html',
-          'app/components/header/header.html',
-          'app/components/home',
-          'app/components/home/home.html',
-          'app/components/home/home.js',
-          'app/images',
-          'app/scripts',
-          'app/scripts/main.js',
-          'app/scripts/config.js',
-          'app/service',
-          'app/service/gruntfiles/gruntfiles.js',
-          'app/styles'
+          expectedDir,
+          expectedApp,
+          expectedConfig,
+          expectedE2e,
+          expectedServer,
+          expectedTest
         ));
-
-        assert.file([].concat(
-          expected,
-          'config',
-          'config/autoprefixer.js',
-          'config/browserSync.js',
-          'config/clean.js',
-          'config/concurrent.js',
-          'config/copy.js',
-          'config/express.js',
-          'config/filerev.js',
-          'config/htmlmin.js',
-          'config/imagemin.js',
-          'config/injector.js',
-          'config/jshint.js',
-          'config/karma.js',
-          'config/plato.js',
-          'config/protractor.js',
-          'config/run.js',
-          'config/svgmin.js',
-          'config/usemin.js',
-          'config/useminPrepare.js',
-          'config/watch.js',
-          'config/wiredep.js',
-          'config/yuidoc.js'
-        ));
-
-        assert.file([].concat(
-          expected,
-          'e2e',
-          'e2e/spec',
-          'e2e/spec/exists.spec.js',
-          'e2e/pageObject.conf.js',
-          'e2e/protractor.conf.js'
-        ));
-
-        assert.file([].concat(
-          expected,
-          'server',
-          'server/api',
-          'server/api/gruntfiles.js',
-          'server/data',
-          'server/data/app.json',
-          'server/app.js'
-        ));
-
-        assert.file([].concat(
-          expected,
-          'test',
-          'test/components',
-          'test/components/about',
-          'test/components/about/about.spec.js',
-          'test/components/contact',
-          'test/components/contact/contact.spec.js',
-          'test/components/home',
-          'test/components/home/home.spec.js',
-          'test/service',
-          'test/service/gruntfiles',
-          'test/service/gruntfiles/gruntfiles.mock.js',
-          'test/service/gruntfiles/gruntfiles.spec.js',
-          'test/karma.conf.js',
-          'test/main.spec.js'
-        ));
-
+        
         assert.fileContent(expectedContent);
+
         assert.noFileContent([
           ['Gruntfile.js', /modernizr/],
           ['app/index.html', /modernizr/],
@@ -172,6 +179,17 @@ describe('Angular Eggs generator', function () {
       runGen.withOptions(options).withPrompt({features: ['includeModernizr']})
       .on('end', function () {
 
+        assert.file([].concat(
+          expected,
+          expectedDir,
+          expectedApp,
+          expectedConfig,
+          expectedE2e,
+          expectedServer,
+          expectedTest,
+          'config/modernizr.js'
+        ));
+
         assert.fileContent([
           ['Gruntfile.js', /modernizr/],
           ['app/index.html', /modernizr/],
@@ -179,10 +197,6 @@ describe('Angular Eggs generator', function () {
           ['package.json', /modernizr/],
         ]);
 
-        assert.file([].concat(
-          expected,
-          'config/modernizr.js'
-        ));
 
         done();
       });
@@ -197,6 +211,16 @@ describe('Angular Eggs generator', function () {
           ['bower.json', /bootstrap/],
           ['config/copy.js', /dest/]
         ]);
+
+        assert.file([].concat(
+          expected,
+          expectedDir,
+          expectedApp,
+          expectedConfig,
+          expectedE2e,
+          expectedServer,
+          expectedTest
+        ));
 
         done();
       });
@@ -219,6 +243,12 @@ describe('Angular Eggs generator', function () {
 
         assert.file([].concat(
           expected,
+          expectedDir,
+          expectedApp,
+          expectedConfig,
+          expectedE2e,
+          expectedServer,
+          expectedTest,
           'app/styles/main.scss'
         ));
 
@@ -245,9 +275,85 @@ describe('Angular Eggs generator', function () {
 
         assert.file([].concat(
           expected,
+          expectedDir,
+          expectedApp,
+          expectedConfig,
+          expectedE2e,
+          expectedServer,
+          expectedTest,
           'app/styles/main.scss'
         ));
 
+        done();
+      });
+    });
+
+    it('creates expected modernizr components', function (done) {
+      runGen.withOptions(options).withPrompt({features: ['includeModernizr']})
+      .on('end', function () {
+
+        assert.file([].concat(
+          expected,
+          expectedDir,
+          expectedApp,
+          expectedConfig,
+          expectedE2e,
+          expectedServer,
+          expectedTest,
+          'config/modernizr.js'
+        ));
+
+        assert.fileContent([
+          ['Gruntfile.js', /modernizr/],
+          ['app/index.html', /modernizr/],
+          ['bower.json', /modernizr/],
+          ['package.json', /modernizr/],
+        ]);
+
+
+        done();
+      });
+    });
+
+    it('creates expected files', function (done) {
+
+      options = {
+        'skip-install-message': false,
+        'skip-install': false,
+        'skip-welcome-message': false,
+        'skip-message': false
+      };
+      runGen.withOptions(options).on('end', function () {
+
+        assert.file([].concat(
+          expected,
+          expectedDir,
+          expectedApp,
+          expectedConfig,
+          expectedE2e,
+          expectedServer,
+          expectedTest
+        ));
+        
+        assert.fileContent(expectedContent);
+
+        assert.noFileContent([
+          ['Gruntfile.js', /modernizr/],
+          ['app/index.html', /modernizr/],
+          ['bower.json', /modernizr/],
+          ['package.json', /modernizr/],
+          ['Gruntfile.js', /bootstrap/],
+          ['app/index.html', /bootstrap/],
+          ['bower.json', /bootstrap/],
+          ['Gruntfile.js', /sass/],
+          ['app/index.html', /Sass/],
+          ['.gitignore', /\.sass-cache/],
+          ['package.json', /grunt-contrib-sass/],
+          ['package.json', /grunt-sass/],
+          ['Gruntfile.js', /bootstrap-sass-official/],
+          ['app/index.html', /Sass is a mature/],
+          ['bower.json', /bootstrap-sass-official/]
+        ]);
         done();
       });
     });
