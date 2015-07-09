f2de151e784d0dec61eac8357d208366c4d4dfde
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-07-09 13:36:54 +0900

    angular1.4.2

--- app/index.js
+++ app/index.js
@@ -90,9 +90,9 @@ module.exports = yeoman.generators.Base.extend({
       bower.dependencies.modernizr = '~2.8.3';
     }
 
-    bower.dependencies.angular = '~1.4.1';
+    bower.dependencies.angular = '~1.4.2';
     bower.dependencies['angular-new-router'] = '~0.5.3';
-    bower.dependencies['angular-resource'] = '~1.4.1';
+    bower.dependencies['angular-resource'] = '~1.4.2';
 
     bower.overrides['angular-new-router'] = {};
     bower.overrides['angular-new-router'].main = 'dist/router.es5.js';
@@ -102,7 +102,7 @@ module.exports = yeoman.generators.Base.extend({
       bower.overrides['bootstrap'].main = 'dist/css/bootstrap.css';
     }
 
-    bower.devDependencies['angular-mocks'] = '~1.4.1';
+    bower.devDependencies['angular-mocks'] = '~1.4.2';
 
     this.copy('bowerrc', '.bowerrc');
     this.write('bower.json', JSON.stringify(bower, null, 2));
